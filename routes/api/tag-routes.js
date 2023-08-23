51f6402b3881c3fa4cd121cdfb828b9aaa1736f0
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-08-23 22:55:29 +1000

    fixed model

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,7 +5,7 @@ const { Tag, Product, ProductTag } = require("../../models");
 
 router.get("/", async (req, res) => {
   try {
-    const tagData = await ProductTag.findAll({
+    const tagData = await Tag.findAll({
       include: [{ model: Product }],
     });
     res.status(200).json(tagData);
@@ -16,7 +16,7 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   try {
-    const tagData = await ProductTag.findByPk(req.params.id, {
+    const tagData = await Tag.findByPk(req.params.id, {
       include: [{ model: Product }],
     });
 
@@ -32,7 +32,7 @@ router.get("/:id", async (req, res) => {
 
 router.post("/", async (req, res) => {
   try {
-    const tagData = await ProductTag.create({
+    const tagData = await Tag.create({
       tag_name: req.body.tag_name,
     });
     res.status(200).json(tagData);
@@ -43,7 +43,7 @@ router.post("/", async (req, res) => {
 
 router.put("/:id", async (req, res) => {
   try {
-    const tagData = await ProductTag.update(req.body, {
+    const tagData = await Tag.update(req.body, {
       where: {
         id: req.params.id,
       },
@@ -57,7 +57,7 @@ router.put("/:id", async (req, res) => {
 
 router.delete("/:id", async (req, res) => {
   try {
-    const tagData = await ProductTag.destroy({
+    const tagData = await Tag.destroy({
       where: {
         id: req.params.id,
       },
