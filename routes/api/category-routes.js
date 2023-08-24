cf639c6732f526b04fa3aa1f014736c361145021
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-08-24 21:47:33 +1000

    finalised category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -21,7 +21,7 @@ router.get("/:id", async (req, res) => {
     });
 
     if (!catData) {
-      res.status(404).json({ message: "No category card found with that id." });
+      res.status(404).json({ message: "No category found with that id." });
       return;
     }
     res.status(200).json(catData);
