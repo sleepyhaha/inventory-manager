250009837f7998f7957a54800f09765ac664b4c8
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-08-24 22:12:01 +1000

    fixed product tag relationship

--- models/index.js
+++ models/index.js
@@ -16,13 +16,19 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag,
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
   foreignKey: "product_id",
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag,
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
   foreignKey: "product_id",
 });
 
